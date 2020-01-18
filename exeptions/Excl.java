e43cef9fc911249794dc94e87a5c3d1e77b1eb3a
DenisProgrammer <30808396+scisoftdev@users.noreply.github.com>
2020-01-18 09:11:00 +0400

    Create Excl.java

---
+++ exeptions/Excl.java
@@ -0,0 +1,16 @@
+package Exeption;
+
+public class Excl {
+    public static void main (String args[]) {
+        int d,a;
+        try {
+            d = 0;
+            a = 42/d;
+            System.out.println("Это не будет выведено");
+        } catch (ArithmeticException e) {
+            System.out.println("Деление на нуль");
+        }
+
+        System.out.println("После оператора catch");
+    }
+}
